f2221eec02997b28fa4a697cb7dd1ba6e7fcc67e
xaita <xiao.shan.costajussa@estudiantat.upc.edu>
2024-02-28 19:03:08 +0100

    Reworking Audio ( Not yet)

--- Ymir Engine/Source/CAudioListener.cpp
+++ Ymir Engine/Source/CAudioListener.cpp
@@ -6,115 +6,82 @@
 #include "Application.h"
 #include "ModuleAudio.h"
 
-//C_AudioListener::C_AudioListener(GameObject* _gm, bool defaultListener) :Component(_gm), isDefaultListener(false), myTransform(nullptr)
-//{
-//	name = "Audio Listener";
-//	myTransform = dynamic_cast<C_Transform*>(gameObject->GetComponent(Component::TYPE::TRANSFORM));
+CAudioListener::CAudioListener(GameObject* _gm, bool defaultListener) :Component(_gm), isDefaultListener(false), myTransform(nullptr)
+{
+	
+	myTransform = dynamic_cast<Cransform*>(gameObject->GetComponent(Component::TYPE::TRANSFORM));
+
+	id = static_cast<unsigned int>(EngineExternal->GetRandomInt());
+	EngineExternal->moduleAudio->RegisterNewAudioObject(id);
+	SetAsDefaultListener(defaultListener);
+}
+
+
+CAudioListener::~CAudioListener()
+{
+	EngineExternal->moduleAudio->UnRegisterAudioObject(id);
+	SetAsDefaultListener(false);
+	myTransform = nullptr;
+}
+
+#ifndef STANDALONE
+bool CAudioListener::OnEditor()
+{
+	if (Component::OnEditor() == true)
+	{
+		ImGui::Separator();
+
+		bool listenerAux = isDefaultListener;
+		if (ImGui::Checkbox("Default Listener", &listenerAux))
+		{
+			SetAsDefaultListener(listenerAux);
+		}
+
+		return true;
+	}
+	return false;
+}
+#endif // !STANDALONE
+
+void CAudioListener::Update()
+{
+	float3 pos = myTransform->mGlobalMatrix.TranslatePart();
+	float3 axis = { 1,0,0 };
+	float3 axis2 = { 1,0,0 };
+	External->audio->SetAudioObjTransform(id, pos, axis, axis2);
+}
 //
-//	id = static_cast<unsigned int>(EngineExternal->GetRandomInt());
-//	EngineExternal->moduleAudio->RegisterNewAudioObject(id);
-//	SetAsDefaultListener(defaultListener);
-//}
-//
-//
-//C_AudioListener::~C_AudioListener()
-//{
-//	EngineExternal->moduleAudio->UnRegisterAudioObject(id);
-//	SetAsDefaultListener(false);
-//	myTransform = nullptr;
-//}
-//
-//#ifndef STANDALONE
-//bool C_AudioListener::OnEditor()
-//{
-//	if (Component::OnEditor() == true)
-//	{
-//		ImGui::Separator();
-//
-//		bool listenerAux = isDefaultListener;
-//		if (ImGui::Checkbox("Default Listener", &listenerAux))
-//		{
-//			SetAsDefaultListener(listenerAux);
-//		}
-//
-//		return true;
-//	}
-//	return false;
-//}
-//#endif // !STANDALONE
-//
-//void C_AudioListener::Update()
-//{
-//	float3 pos = myTransform->globalTransform.TranslatePart();
-//	EngineExternal->moduleAudio->SetAudioObjTransform(id, pos, myTransform->GetForward(), myTransform->GetUp());
-//}
-//
-//void C_AudioListener::SaveData(JSON_Object* nObj)
+//void CAudioListener::SaveData(JSON_Object* nObj)
 //{
 //	Component::SaveData(nObj);
 //	DEJson::WriteBool(nObj, "isDefaultListener", isDefaultListener);
 //
 //}
 //
-//void C_AudioListener::LoadData(DEConfig& nObj)
+//void CAudioListener::LoadData(JSON_Object& nObj)
 //{
 //	Component::LoadData(nObj);
 //	SetAsDefaultListener(nObj.ReadBool("isDefaultListener"));
 //
 //}
-//
-//
-//
-//void C_AudioListener::SetVolume(float newVol)
-//{
-//	newVol = MIN(newVol, 99.99f);
-//	newVol = MAX(newVol, 0.0f);
-//
-//	if (isDefaultListener)
-//		EngineExternal->moduleAudio->SetBusVolume(newVol);
-//}
+
+
+
+void CAudioListener::SetVolume(float newVol)
+{
+	newVol = MIN(newVol, 99.99f);
+	newVol = MAX(newVol, 0.0f);
+
+	if (isDefaultListener)
+		External->audio->SetBusVolume(newVol);
+}
 //
 //uint C_AudioListener::GetID()
 //{
 //	return id;
 //}
 //
-//void C_AudioListener::SetID(uint id)//TODO will it be possible to change Component ID after initialization? if not delete SetID()
-//{
-//	EngineExternal->moduleAudio->UnRegisterAudioObject(this->id);
-//	this->id = id;
-//	EngineExternal->moduleAudio->RegisterNewAudioObject(id);
-//}
-//
-//void C_AudioListener::SetAsDefaultListener(bool setDefault)
-//{
-//	if (setDefault)
-//	{
-//		if (EngineExternal->moduleAudio->defaultListener != nullptr)
-//		{
-//			if (EngineExternal->moduleAudio->defaultListener != this)
-//			{
-//				EngineExternal->moduleAudio->defaultListener->SetAsDefaultListener(false);
-//			}
-//
-//		}
-//		EngineExternal->moduleAudio->defaultListener = this;
-//		EngineExternal->moduleAudio->WwiseListnerHasToUpdate();
-//
-//
-//	}
-//	else if (EngineExternal->moduleAudio->defaultListener != nullptr)
-//	{
-//		if (EngineExternal->moduleAudio->defaultListener == this)
-//		{
-//			EngineExternal->moduleAudio->defaultListener = nullptr;
-//			EngineExternal->moduleAudio->WwiseListnerHasToUpdate();
-//		}
-//	}
-//
-//	isDefaultListener = setDefault;
-//	SetVolume(EngineExternal->moduleAudio->masterVolume);
-//}
+
 
 CAudioListener::CAudioListener(GameObject* owner, bool defaultListener)
 {
@@ -143,17 +110,44 @@ void CAudioListener::SetVolume(float newVol)
 
 uint CAudioListener::GetID()
 {
-	return uint();
+	return id;
 }
 
 void CAudioListener::SetID(uint id)
 {
-
+	//	EngineExternal->moduleAudio->UnRegisterAudioObject(this->id);
+//	this->id = id;
+//	EngineExternal->moduleAudio->RegisterNewAudioObject(id);
 }
 
 void CAudioListener::SetAsDefaultListener(bool setDefault)
 {
-
+	//	if (setDefault)
+//	{
+//		if (EngineExternal->moduleAudio->defaultListener != nullptr)
+//		{
+//			if (EngineExternal->moduleAudio->defaultListener != this)
+//			{
+//				EngineExternal->moduleAudio->defaultListener->SetAsDefaultListener(false);
+//			}
+//
+//		}
+//		EngineExternal->moduleAudio->defaultListener = this;
+//		EngineExternal->moduleAudio->WwiseListnerHasToUpdate();
+//
+//
+//	}
+//	else if (EngineExternal->moduleAudio->defaultListener != nullptr)
+//	{
+//		if (EngineExternal->moduleAudio->defaultListener == this)
+//		{
+//			EngineExternal->moduleAudio->defaultListener = nullptr;
+//			EngineExternal->moduleAudio->WwiseListnerHasToUpdate();
+//		}
+//	}
+//
+//	isDefaultListener = setDefault;
+//	SetVolume(EngineExternal->moduleAudio->masterVolume);
 }
 
 bool CAudioListener::IsDefaultListener()
