86db0496b25a4642e9b06dc5ffa412044ababd0e
Mario Garcia Sutil <mariosutil2003@gmail.com>
2024-02-29 19:30:34 +0100

    Engine can load our SoundBanks!!!

--- Ymir Engine/Game/Assets/SoundBanks/Wwise_IDs.h
+++ Ymir Engine/Game/Assets/SoundBanks/Wwise_IDs.h
@@ -13,12 +13,13 @@ namespace AK
 {
     namespace EVENTS
     {
-        static const AkUniqueID ACTIVATEBGMUSIC = 799949012U;
+        static const AkUniqueID JV = 1735860057U;
     } // namespace EVENTS
 
     namespace GAME_PARAMETERS
     {
-        static const AkUniqueID PITCH = 1908158473U;
+        static const AkUniqueID PLAYBACK_RATE = 1524500807U;
+        static const AkUniqueID RPM = 796049864U;
         static const AkUniqueID SS_AIR_FEAR = 1351367891U;
         static const AkUniqueID SS_AIR_FREEFALL = 3002758120U;
         static const AkUniqueID SS_AIR_FURY = 1029930033U;
@@ -29,22 +30,23 @@ namespace AK
         static const AkUniqueID SS_AIR_STORM = 3715662592U;
         static const AkUniqueID SS_AIR_TIMEOFDAY = 3203397129U;
         static const AkUniqueID SS_AIR_TURBULENCE = 4160247818U;
-        static const AkUniqueID VOLUME = 2415836739U;
     } // namespace GAME_PARAMETERS
 
     namespace BANKS
     {
         static const AkUniqueID INIT = 1355168291U;
-        static const AkUniqueID RAGNARENGINE = 87917746U;
+        static const AkUniqueID FUNCIONAPLS = 6333691U;
     } // namespace BANKS
 
     namespace BUSSES
     {
         static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID MOTION_FACTORY_BUS = 985987111U;
     } // namespace BUSSES
 
     namespace AUDIO_DEVICES
     {
+        static const AkUniqueID DEFAULT_MOTION_DEVICE = 4230635974U;
         static const AkUniqueID NO_OUTPUT = 2317455096U;
         static const AkUniqueID SYSTEM = 3859886410U;
     } // namespace AUDIO_DEVICES
