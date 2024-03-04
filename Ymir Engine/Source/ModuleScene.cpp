7901679d89a6122089bc9835a532ab572cbd015e
Mario Garcia Sutil <mariosutil2003@gmail.com>
2024-03-04 12:11:18 +0100

    Camera as AudioSource

--- Ymir Engine/Source/ModuleScene.cpp
+++ Ymir Engine/Source/ModuleScene.cpp
@@ -27,8 +27,8 @@ ModuleScene::ModuleScene(Application* app, bool start_enabled) : Module(app, sta
 
 	gameCameraObject = CreateGameObject("Main Camera", mRootNode);
 	gameCameraObject->UID = Random::Generate();
-	audiosource = CreateGameObject("AudioSource", mRootNode);
-	audiosource->UID = Random::Generate();
+	//audiosource = CreateGameObject("AudioSource", mRootNode);
+	//audiosource->UID = Random::Generate();
 
 	gameCameraComponent = nullptr;
 
@@ -49,9 +49,11 @@ bool ModuleScene::Init()
 	CAudioListener* audioListenerComponent = new CAudioListener(gameCameraObject);
 	audioListenerComponent->SetAsDefaultListener();
 	gameCameraObject->AddComponent(audioListenerComponent);
+	CAudioSource* audioSourceComponent = new CAudioSource(gameCameraObject);
+	gameCameraObject->AddComponent(audioSourceComponent);
 
-	CAudioSource* audioSourceComponent = new CAudioSource(audiosource);
-	audiosource->AddComponent(audioSourceComponent);
+	//CAudioSource* audioSourceComponent2 = new CAudioSource(audiosource);
+	//audiosource->AddComponent(audioSourceComponent2);
 
 	// yscene file creation
 
