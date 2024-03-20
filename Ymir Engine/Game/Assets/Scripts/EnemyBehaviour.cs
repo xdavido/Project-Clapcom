07c5efc128a061e297172c2de4db2decd2019cce
Maksym203 <maksym.polupan@estudiantat.upc.edu>
2024-03-20 18:40:30 +0100

    Rotate working (but weird)

--- Ymir Engine/Game/Assets/Scripts/EnemyBehaviour.cs
+++ Ymir Engine/Game/Assets/Scripts/EnemyBehaviour.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
@@ -51,7 +52,13 @@ public class EnemyBehaviour : YmirComponent
 
     public float xSpeed = 0, ySpeed = 0;
 
-    private int counter = 0;
+    private int counter1 = 0, counter2 = 0;
+
+    private double angle = 0.0f;
+
+    public float rotationSpeed = 0.1f;
+
+    private bool rotate = false;
 
     //private EnemyState state = EnemyState.Idle;
 
@@ -74,22 +81,92 @@ public class EnemyBehaviour : YmirComponent
             (xSpeed, ySpeed) = pointGenerator.GetRandomPointInRadius(wanderRadius);
 
             wanderState = WanderState.Going;
+            rotate = true;
             //state = EnemyState.Moving;
         }
 
         if (wanderState == WanderState.Going)
         {
-            //Doing both in the same line doesn't work right
             gameObject.transform.localPosition += new Vector3(xSpeed, 0, 0) * movementSpeed * Time.deltaTime;
             gameObject.transform.localPosition += new Vector3(0, 0, ySpeed) * movementSpeed * Time.deltaTime;
 
-            counter++;
+            counter1++;
 
-            if (counter > 500)
+            if (counter1 > 500)
             {
                 wanderState = WanderState.Reached;
-                counter = 0;
+                counter1 = 0;
             }
+
+            //Rotation
+
+            //Vector3 X = new Vector3(xSpeed, 0, -ySpeed - 1);
+            //Vector3 Y = new Vector3(0, 0, 1);
+            //X = Vector3.Normalize(X);
+
+            //if (X.x >= 0)
+            //{
+            //    angle = Math.Acos(Vector3.Dot(X, Y) - 1);
+            //}
+            //else if (X.x < 0)
+            //{
+            //    angle = -Math.Acos(Vector3.Dot(X, Y) - 1);
+            //}
+
+            //angle += 0f; //Add offset here if needed
+            //Se viene hardcodeada
+
+            if (rotate == true)
+            {
+                if (counter2 == 0)
+                {
+                    Vector3 X = new Vector3(xSpeed, 0, -ySpeed - 1);
+                    Vector3 Y = new Vector3(0, 0, 1);
+                    X = Vector3.Normalize(X);
+
+                    if (X.x >= 0)
+                    {
+                        angle = Math.Acos(Vector3.Dot(X, Y) - 1);
+                    }
+                    else if (X.x < 0)
+                    {
+                        angle = -Math.Acos(Vector3.Dot(X, Y) - 1);
+                    }
+
+                    angle = angle / 20;
+                    counter2++;
+
+                }
+
+                if (counter2 > 0)
+                {
+                    counter2++;
+                    angle = angle + (angle / counter2);
+                    gameObject.transform.localRotation = Quaternion.RotateAroundAxis(Vector3.up, (float)-angle);
+                    if (counter2 > 20)
+                    {
+                        counter2 = 0;
+                        rotate = false;
+                        Vector3 X = new Vector3(xSpeed, 0, -ySpeed - 1);
+                        Vector3 Y = new Vector3(0, 0, 1);
+                        X = Vector3.Normalize(X);
+
+                        if (X.x >= 0)
+                        {
+                            angle = Math.Acos(Vector3.Dot(X, Y) - 1);
+                        }
+                        else if (X.x < 0)
+                        {
+                            angle = -Math.Acos(Vector3.Dot(X, Y) - 1);
+                        }
+                        gameObject.transform.localRotation = Quaternion.RotateAroundAxis(Vector3.up, (float)-angle);
+
+                    }
+
+                }
+            }
+
+            //gameObject.transform.localRotation = Quaternion.RotateAroundAxis(Vector3.up, (float)-angle);
 
         }
     }
