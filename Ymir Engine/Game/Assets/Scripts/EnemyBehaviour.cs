19883c41c6835dc3a322a7d17a0299ac9f211998
Maksym203 <maksym.polupan@estudiantat.upc.edu>
2024-03-20 20:07:59 +0100

    Physics to enemy, but no rotation

--- Ymir Engine/Game/Assets/Scripts/EnemyBehaviour.cs
+++ Ymir Engine/Game/Assets/Scripts/EnemyBehaviour.cs
@@ -44,7 +44,7 @@ public class EnemyBehaviour : YmirComponent
 
     private bool start = true;
 
-    public float movementSpeed = 0.1f;
+    public float movementSpeed = 0.2f;
 
     public float life = 100f;
 
@@ -52,13 +52,15 @@ public class EnemyBehaviour : YmirComponent
 
     public float xSpeed = 0, ySpeed = 0;
 
-    private int counter1 = 0, counter2 = 0;
+    private int counter1 = 0;
+    
+    //private int counter2 = 0;
 
-    private double angle = 0.0f;
+    //private double angle = 0.0f;
 
-    public float rotationSpeed = 0.1f;
+    //public float rotationSpeed = 0.1f;
 
-    private bool rotate = false;
+    //private bool rotate = false;
 
     //private EnemyState state = EnemyState.Idle;
 
@@ -81,14 +83,13 @@ public class EnemyBehaviour : YmirComponent
             (xSpeed, ySpeed) = pointGenerator.GetRandomPointInRadius(wanderRadius);
 
             wanderState = WanderState.Going;
-            rotate = true;
+            //rotate = true;
             //state = EnemyState.Moving;
         }
 
         if (wanderState == WanderState.Going)
         {
-            gameObject.transform.localPosition += new Vector3(xSpeed, 0, 0) * movementSpeed * Time.deltaTime;
-            gameObject.transform.localPosition += new Vector3(0, 0, ySpeed) * movementSpeed * Time.deltaTime;
+            gameObject.SetVelocity(new Vector3(xSpeed, -5, ySpeed) * movementSpeed);
 
             counter1++;
 
@@ -98,75 +99,58 @@ public class EnemyBehaviour : YmirComponent
                 counter1 = 0;
             }
 
-            //Rotation
+            ////Rotation
+            ////Se viene hardcodeada
 
-            //Vector3 X = new Vector3(xSpeed, 0, -ySpeed - 1);
-            //Vector3 Y = new Vector3(0, 0, 1);
-            //X = Vector3.Normalize(X);
-
-            //if (X.x >= 0)
+            //if (rotate == true)
             //{
-            //    angle = Math.Acos(Vector3.Dot(X, Y) - 1);
+            //    if (counter2 == 0)
+            //    {
+            //        Vector3 X = new Vector3(xSpeed, 0, -ySpeed - 1);
+            //        Vector3 Y = new Vector3(0, 0, 1);
+            //        X = Vector3.Normalize(X);
+
+            //        if (X.x >= 0)
+            //        {
+            //            angle = Math.Acos(Vector3.Dot(X, Y) - 1);
+            //        }
+            //        else if (X.x < 0)
+            //        {
+            //            angle = -Math.Acos(Vector3.Dot(X, Y) - 1);
+            //        }
+
+            //        angle = angle / 20;
+            //        counter2++;
+
+            //    }
+
+            //    if (counter2 > 0)
+            //    {
+            //        counter2++;
+            //        angle = angle + (angle / counter2);
+            //        gameObject.transform.localRotation = Quaternion.RotateAroundAxis(Vector3.up, (float)-angle);
+            //        if (counter2 > 20)
+            //        {
+            //            counter2 = 0;
+            //            rotate = false;
+            //            Vector3 X = new Vector3(xSpeed, 0, -ySpeed - 1);
+            //            Vector3 Y = new Vector3(0, 0, 1);
+            //            X = Vector3.Normalize(X);
+
+            //            if (X.x >= 0)
+            //            {
+            //                angle = Math.Acos(Vector3.Dot(X, Y) - 1);
+            //            }
+            //            else if (X.x < 0)
+            //            {
+            //                angle = -Math.Acos(Vector3.Dot(X, Y) - 1);
+            //            }
+            //            gameObject.transform.localRotation = Quaternion.RotateAroundAxis(Vector3.up, (float)-angle);
+
+            //        }
+
+            //    }
             //}
-            //else if (X.x < 0)
-            //{
-            //    angle = -Math.Acos(Vector3.Dot(X, Y) - 1);
-            //}
-
-            //angle += 0f; //Add offset here if needed
-            //Se viene hardcodeada
-
-            if (rotate == true)
-            {
-                if (counter2 == 0)
-                {
-                    Vector3 X = new Vector3(xSpeed, 0, -ySpeed - 1);
-                    Vector3 Y = new Vector3(0, 0, 1);
-                    X = Vector3.Normalize(X);
-
-                    if (X.x >= 0)
-                    {
-                        angle = Math.Acos(Vector3.Dot(X, Y) - 1);
-                    }
-                    else if (X.x < 0)
-                    {
-                        angle = -Math.Acos(Vector3.Dot(X, Y) - 1);
-                    }
-
-                    angle = angle / 20;
-                    counter2++;
-
-                }
-
-                if (counter2 > 0)
-                {
-                    counter2++;
-                    angle = angle + (angle / counter2);
-                    gameObject.transform.localRotation = Quaternion.RotateAroundAxis(Vector3.up, (float)-angle);
-                    if (counter2 > 20)
-                    {
-                        counter2 = 0;
-                        rotate = false;
-                        Vector3 X = new Vector3(xSpeed, 0, -ySpeed - 1);
-                        Vector3 Y = new Vector3(0, 0, 1);
-                        X = Vector3.Normalize(X);
-
-                        if (X.x >= 0)
-                        {
-                            angle = Math.Acos(Vector3.Dot(X, Y) - 1);
-                        }
-                        else if (X.x < 0)
-                        {
-                            angle = -Math.Acos(Vector3.Dot(X, Y) - 1);
-                        }
-                        gameObject.transform.localRotation = Quaternion.RotateAroundAxis(Vector3.up, (float)-angle);
-
-                    }
-
-                }
-            }
-
-            //gameObject.transform.localRotation = Quaternion.RotateAroundAxis(Vector3.up, (float)-angle);
 
         }
     }
