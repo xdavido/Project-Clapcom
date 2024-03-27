493121442b1b7de93838d46644f4db06d1f80a69
BielRubio <bielrudi@gmail.com>
2024-03-27 21:11:02 +0100

    Update Animation.cs

--- Ymir Engine/Game/Assembly-CSharp/Ymir Engine Utility/Animation.cs
+++ Ymir Engine/Game/Assembly-CSharp/Ymir Engine Utility/Animation.cs
@@ -34,31 +34,41 @@ namespace YmirEngine
         public static extern void StopAnimation(object go);
 
         /// <summary>
-        /// Set animation to loop when played. If not given an animation name it applies to all animations in GameObject.
+        /// Set animation to loop when played. If not given <c>animationName</c> it applies to all animations in GameObject.
         /// </summary>
         [MethodImplAttribute(MethodImplOptions.InternalCall)]
         public static extern void SetLoop(object go, string animationName, bool loop);
 
         /// <summary>
-        /// Set animation to play blackawards. If not given an animation name it applies to all animations in GameObject.
+        /// Set animation to play blackawards. If not given <c>animationName</c> it applies to all animations in GameObject.
         /// </summary>
         [MethodImplAttribute(MethodImplOptions.InternalCall)]
         public static extern void SetBackward(object go, string animationName, bool backwards);
 
         /// <summary>
-        /// Set animation to ping-pong when played. If not given an animation name it applies to all animations in GameObject.
+        /// Set animation to ping-pong when played. If not given <c>animationName</c> it applies to all animations in GameObject.
         /// </summary>
         [MethodImplAttribute(MethodImplOptions.InternalCall)]
         public static extern void SetPingPong(object go, string animationName, bool pingPong);
 
         /// <summary>
-        /// Set animation speed. If not given a name it applies to all animations in GameObject.
+        /// Set animation speed. If not given <c>animationName</c> it applies to all animations in GameObject.
         /// </summary>
         [MethodImplAttribute(MethodImplOptions.InternalCall)]
         public static extern void SetSpeed(object go, string animationName, float speed);
 
         /// <summary>
-        /// Specify which animations will blend into 'animationName' and the duration of the blend. If not given an animation name it applies to all animations.
+        /// Specify if <c>animationName</c> will blend  with <c>blendAnimation</c> when changing to it and the duration of the blend. If not given <c>animationName</c> it applies to all animations.
+        /// <example>
+        /// <para /> For exmaple:
+        /// <code>
+        /// <c>public void Start()</c> <para />
+        /// { <para />
+        /// <c>Animation.AddBlendOption(gameObject, "Walk", "Shoot", 5.0f);</c> <para />
+        /// <c>Animation.AddBlendOption(gameObject, "Walk", "Dash", 2.5f);</c> <para />
+        /// }
+        /// </code>
+        /// </example>
         /// </summary>
         [MethodImplAttribute(MethodImplOptions.InternalCall)]
         public static extern void AddBlendOption(object go, string animationName  = "", string blendName = "", float transitionTime = 10.0f);
