f1a6850074cbe9600d1d03bda9038659c5723532
Eric <ericluque2003@gmail.com>
2024-04-08 23:01:02 +0200

    Loot_Chest provisional

---
+++ Ymir Engine/Game/Assets/Scripts/Loot_Chest.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+using YmirEngine;
+
+public class Loot_Chest : YmirComponent
+{
+	public GameObject item;
+	public void Start()
+	{
+		List<GameObject> list = new List<GameObject>();
+		
+	}
+
+	public void Update()
+	{
+		return;
+	}
+}
